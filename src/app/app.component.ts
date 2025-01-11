a5e962afbe62055a6369af9599cfca8af07d8376
shuaib <shuaib.rasheed@datahq.ai>
2025-01-11 10:26:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, HostListener } from '@angular/core';   
+import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AboutComponent } from "./about/about.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, SkillsComponent, CommonModule, FormsModule, FooterComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'abu-portfolio';
+export class AppComponent implements AfterViewInit{
+  title = 'Shuaib';
+  contactData = {
+    name: '',
+    email: '',
+    message: ''
+  };
+  
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const sections = document.querySelectorAll('.section');
+    const scrollPosition = window.scrollY + window.innerHeight;
+
+    sections.forEach(section => {
+      if ((section as HTMLElement).offsetTop < scrollPosition) {
+        section.classList.add('visible');
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    this.onScroll(); // Trigger visibility on initial load as well
+  }
+
+  onSubmit() {
+    console.log('Form submitted', this.contactData);
+  }
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+  }
+}
